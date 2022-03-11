1301cf4c56325aa13924dfecdd4f44f707791251
imgaram <s21049@gsm.hs.kr>
2022-03-11 14:13:55 +0900

    [CREATE] 화면 전환 이벤트

--- app/src/main/java/com/example/dietmanagement/login/LoginFragment.kt
+++ app/src/main/java/com/example/dietmanagement/login/LoginFragment.kt
@@ -1,22 +1,29 @@
 package com.example.dietmanagement.login
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.dietmanagement.MainActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.dietmanagement.R
 import com.example.dietmanagement.databinding.FragmentLoginBinding
 
-class LoginFragment : AppCompatActivity() {
+class LoginFragment : Fragment() {
 
-    private lateinit var binding: FragmentLoginBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = FragmentLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.nextBtn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val nextBtn = view.findViewById<Button>(R.id.next_btn)
+        nextBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment2_to_signInFragment)
         }
+
+        return view
     }
 }
