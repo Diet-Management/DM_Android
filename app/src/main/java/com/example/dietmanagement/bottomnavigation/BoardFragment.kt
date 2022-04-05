8de06df78cf7706821e8c55b2b3db66de96da56d
imgaram <s21049@gsm.hs.kr>
2022-04-05 19:34:24 +0900

    [CREATE] post 작성 activity 디자인, 화면 전환

--- app/src/main/java/com/example/dietmanagement/bottomnavigation/BoardFragment.kt
+++ app/src/main/java/com/example/dietmanagement/bottomnavigation/BoardFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.navigation.Navigation
 import com.example.dietmanagement.R
 import com.example.dietmanagement.databinding.FragmentBoardBinding
 
@@ -17,11 +18,11 @@ class BoardFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         binding = FragmentBoardBinding.inflate(inflater, container, false)
 
         binding.addPostButton.setOnClickListener {
-
+            Navigation.findNavController(binding.root).navigate(R.id.action_boardFragment_to_addPostActivity)
         }
         return binding.root
     }
