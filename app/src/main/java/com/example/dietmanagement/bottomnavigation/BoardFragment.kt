f3e22ba6510b5e0e9991b6cd65edf281f1d175c9
imgaram <s21049@gsm.hs.kr>
2022-04-09 15:35:38 +0900

    [CREATE] post 정렬 기준 dialog

--- app/src/main/java/com/example/dietmanagement/bottomnavigation/BoardFragment.kt
+++ app/src/main/java/com/example/dietmanagement/bottomnavigation/BoardFragment.kt
@@ -1,13 +1,10 @@
 package com.example.dietmanagement.bottomnavigation
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.dietmanagement.R
 import com.example.dietmanagement.databinding.FragmentBoardBinding
@@ -24,6 +21,15 @@ class BoardFragment : Fragment() {
         binding.addPostButton.setOnClickListener {
             Navigation.findNavController(binding.root).navigate(R.id.action_boardFragment_to_addPostActivity)
         }
+
+        binding.settingStandard.setOnClickListener {
+            setStandard()
+        }
         return binding.root
+    }
+
+    // TODO :: 기준 작성 다이얼로그 만들기
+    private fun setStandard() {
+//        val dialog
     }
 }
