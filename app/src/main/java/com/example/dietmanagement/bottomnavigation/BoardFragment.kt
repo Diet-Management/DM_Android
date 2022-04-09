c7d572a751332d70192825f451301ed7fb3031b0
imgaram <s21049@gsm.hs.kr>
2022-04-09 15:56:29 +0900

    [UPDATE] dialog 디자인, dialog 열기

--- app/src/main/java/com/example/dietmanagement/bottomnavigation/BoardFragment.kt
+++ app/src/main/java/com/example/dietmanagement/bottomnavigation/BoardFragment.kt
@@ -1,9 +1,14 @@
 package com.example.dietmanagement.bottomnavigation
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.dietmanagement.R
@@ -12,6 +17,7 @@ import com.example.dietmanagement.databinding.FragmentBoardBinding
 class BoardFragment : Fragment() {
 
     private lateinit var binding: FragmentBoardBinding
+    private lateinit var dialog: Dialog
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -30,6 +36,19 @@ class BoardFragment : Fragment() {
 
     // TODO :: 기준 작성 다이얼로그 만들기
     private fun setStandard() {
-//        val dialog
+        dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.custom_dialog_set_standard)
+
+        dialog.show()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        dialog.findViewById<AppCompatButton>(R.id.standard_apply).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.findViewById<AppCompatButton>(R.id.standard_cancel).setOnClickListener {
+            dialog.dismiss()
+        }
     }
 }
