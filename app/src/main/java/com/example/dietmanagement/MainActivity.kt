1c94f16d4df38d7a0a76def1b172ca3774153fe5
imgaram <s21049@gsm.hs.kr>
2022-05-01 17:28:35 +0900

    [UPDATE] bottom navigation

--- app/src/main/java/com/example/dietmanagement/MainActivity.kt
+++ app/src/main/java/com/example/dietmanagement/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.dietmanagement
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.example.dietmanagement.bottomnavigation.AlertFragment
+import com.example.dietmanagement.bottomnavigation.BoardFragment
+import com.example.dietmanagement.bottomnavigation.MainFragment
+import com.example.dietmanagement.bottomnavigation.ProfileFragment
 import com.example.dietmanagement.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
 
     lateinit var nav: BottomNavigationView
     private lateinit var binding: ActivityMainBinding
@@ -20,36 +24,61 @@ class MainActivity : AppCompatActivity(){
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
-        nav = binding.bottomNavigationView
-        setBottomNav(navHostFragment)
+        supportFragmentManager.beginTransaction().add(R.id.linear_main, MainFragment()).commit()
+
+        val fragment = MainFragment()
+        val memberIdx = intent.getStringExtra("memberIdx")
+        if (memberIdx != null) {
+            fragment.apply {
+                this.arguments = savedInstanceState.apply {
+                    this?.putString("memberIdx", memberIdx.toString())
+                }
+            }
+            Log.d("INTENT", "onCreate intent putExtra: $memberIdx")
+        }
+
         popUpMenu()
 
-        val navController = navHostFragment.navController
         binding.profileImage.setOnClickListener {
-            navController.navigateUp()
-            navController.navigate(R.id.profileFragment)
+
         }
     }
 
-    private fun setBottomNav(navHost: NavHostFragment) {
-        val navController = navHost.navController
-        nav.setupWithNavController(navController)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.homeFragment -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linear_main, MainFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.boardFragment -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linear_main, BoardFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.alertFragment -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linear_main, AlertFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.profileFragment -> {
+                supportFragmentManager.beginTransaction().replace(R.id.linear_main, ProfileFragment()).commitAllowingStateLoss()
+                return true
+            }
+        }
+        return false
     }
 
     private fun popUpMenu() {
         binding.moreVertImage.setOnClickListener {
             val popup = PopupMenu(applicationContext, it)
             menuInflater.inflate(R.menu.more_vert_menu, popup.menu)
-            popup.setOnMenuItemClickListener(object: PopupMenu.OnMenuItemClickListener {
-                override fun onMenuItemClick(p0: MenuItem?): Boolean {
-                    when(p0?.itemId) {
-                        R.id.logout -> finish()
-                    }
-                    return false
+            popup.setOnMenuItemClickListener { p0 ->
+                when (p0?.itemId) {
+                    R.id.logout -> finish()
                 }
-            })
+                false
+            }
             popup.show()
         }
     }
+
 }
--- data/src/main/java/com/example/data/base/Base.kt
+++ data/src/main/java/com/example/data/base/Base.kt
@@ -1,5 +1,4 @@
 package com.example.data.base
-
 object Base {
-    const val BASE_URL = "http://10.53.68.150:8081/"
+    const val BASE_URL = "http://10.53.68.194:8081/"
 }
