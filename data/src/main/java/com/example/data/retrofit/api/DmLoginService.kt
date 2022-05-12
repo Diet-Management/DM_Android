e6016499019620965536f5024e21c8fa9366d567
imgaram <s21049@gsm.hs.kr>
2022-05-12 09:57:32 +0900

    [CREATE] login api service

--- data/src/main/java/com/example/data/retrofit/api/DmLoginService.kt
+++ data/src/main/java/com/example/data/retrofit/api/DmLoginService.kt
@@ -1,6 +1,7 @@
 package com.example.data.retrofit.api
 
 import com.example.data.data.request.LoginData
+import com.example.data.data.response.BaseResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
@@ -10,5 +11,5 @@ interface DmLoginService {
     @POST("v1/member/login")
     fun loginResponse (
         @Body login: LoginData
-    ): Call<ResponseBody>
+    ): Call<BaseResponse>
 }
