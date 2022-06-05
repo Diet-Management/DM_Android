859bea3025393ea0d1eae2ecad175a2f5f29bbd0
imgaram <s21049@gsm.hs.kr>
2022-06-05 16:43:00 +0900

    [CREATE] 회원 프로필 정보 api service

---
+++ data/src/main/java/com/example/data/retrofit/api/DmProfileInfoService.kt
@@ -0,0 +1,16 @@
+package com.example.data.retrofit.api
+
+import com.example.data.data.response.BaseResponse
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.*
+
+interface DmProfileInfoService {
+    @Multipart
+    @PATCH("/v1/member/profile")
+    fun getProfile(
+        @Header("Authorization") accessToken: String,
+        @Header("RefreshToken") refreshToken: String,
+        @Part key: MultipartBody.Part
+    ): Response<BaseResponse>
+}
