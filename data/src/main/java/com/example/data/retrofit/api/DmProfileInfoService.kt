1896bc0c56319077e64ea9b2e6792d5ce8a27dd5
imgaram <s21049@gsm.hs.kr>
2022-07-06 17:46:31 +0900

    [UPDATE] 회원 프로필 정보 api service 수정

--- data/src/main/java/com/example/data/retrofit/api/DmProfileInfoService.kt
+++ data/src/main/java/com/example/data/retrofit/api/DmProfileInfoService.kt
@@ -3,13 +3,12 @@ package com.example.data.retrofit.api
 import com.example.data.data.response.BaseResponse
 import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.*
 
 interface DmProfileInfoService {
     @Multipart
     @PATCH("/v1/member/profile")
-    fun getProfile(
+    fun setProfile(
         @Header("Authorization") accessToken: String,
         @Header("RefreshToken") refreshToken: String,
         @Part file: MultipartBody.Part
